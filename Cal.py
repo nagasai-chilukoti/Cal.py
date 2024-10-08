bb517d40edf1dab2d017fce91220ea3e8cceb51e
NAGASAI_CHILUKOTI <nagasaichilukoti71@gmail.com>
2024-10-08 13:21:58 +0530

    Update Cal.py

--- Cal.py
+++ Cal.py
@@ -1,53 +1,37 @@
-from tkinter import *
-
-window = Tk()
-window.title("Simple Calculator")
-window.geometry("400x450")
-window.config(bg="light grey")
-
-equation_str = ""
-
-def show_value(value):
-    global equation_str
-    equation_str += value
-    label_display.config(text=equation_str)
-
-def clear():
-    global equation_str
-    equation_str = ""
-    label_display.config(text=equation_str)
-
-def calculate_result():
-    global equation_str
-    result = ""
-    if equation_str != "":
-        try:
-            result = eval(equation_str)
-        except:
-            result = "Error"
-            equation_str = ""
-        label_display.config(text=result)
-
-label_display = Label(window, width=25, height=3, font=("Courier", 20, "underline"))
-label_display.place(x=1, y=0)
-
-Button(window, text="C", bg="red", fg="white", font=("Arial", 20, "underline"), width=5, height=1, relief=RAISED, bd=2, command=clear).place(x=10, y=110)
-Button(window, text="4", bg="#B3B6B7", fg="white", font=("Arial", 20, "bold"), width=5, height=1, relief=RAISED, bd=2, command=lambda: show_value('4')).place(x=10, y=250)
-Button(window, text="5", bg="#B3B6B7", fg="white", font=("Arial", 20, "bold"), width=5, height=1, relief=RAISED, bd=2, command=lambda: show_value('5')).place(x=110, y=250)
-Button(window, text="6", bg="#B3B6B7", fg="white", font=("Arial", 20, "bold"), width=5, height=1, relief=RAISED, bd=2, command=lambda: show_value('6')).place(x=210, y=250)
-Button(window, text="+", bg="#2a2d36", fg="white", font=("Arial", 20, "bold"), width=5, height=1, relief=RAISED, bd=2, command=lambda: show_value('+')).place(x=310, y=250)
-Button(window, text="/", bg="#2a2d36", fg="white", font=("Arial", 20, "bold"), width=5, height=1, relief=RAISED, bd=2, command=lambda: show_value('/')).place(x=110, y=110)
-Button(window, text="%", bg="#2a2d36", fg="white", font=("Arial", 20, "bold"), width=5, height=1, relief=RAISED, bd=2, command=lambda: show_value('%')).place(x=210, y=110)
-Button(window, text="*", bg="#2a2d36", fg="white", font=("Arial", 20, "bold"), width=5, height=1, relief=RAISED, bd=2, command=lambda: show_value('*')).place(x=310, y=110)
-Button(window, text="1", bg="#B3B6B7", fg="white", font=("Arial", 20, "bold"), width=5, height=1, relief=RAISED, bd=2, command=lambda: show_value('1')).place(x=10, y=320)
-Button(window, text="2", bg="#B3B6B7", fg="white", font=("Arial", 20, "bold"), width=5, height=1, relief=RAISED, bd=2, command=lambda: show_value('2')).place(x=110, y=320)
-Button(window, text="3", bg="#B3B6B7", fg="white", font=("Arial", 20, "bold"), width=5, height=1, relief=RAISED, bd=2, command=lambda: show_value('3')).place(x=210, y=320)
-Button(window, text="=", bg="#EC7063", fg="white", font=("Arial", 20, "bold"), width=5, height=3, relief=RAISED, bd=2, command=calculate_result).place(x=310, y=323)
-Button(window, text="7", bg="#B3B6B7", fg="white", font=("Arial", 20, "bold"), width=5, height=1, relief=RAISED, bd=2, command=lambda: show_value('7')).place(x=10, y=180)
-Button(window, text="8", bg="#B3B6B7", fg="white", font=("Arial", 20, "bold"), width=5, height=1, relief=RAISED, bd=2, command=lambda: show_value('8')).place(x=110, y=180)
-Button(window, text="9", bg="#B3B6B7", fg="white", font=("Arial", 20, "bold"), width=5, height=1, relief=RAISED, bd=2, command=lambda: show_value('9')).place(x=210, y=180)
-Button(window, text="-", bg="#2a2d36", fg="white", font=("Arial", 20, "bold"), width=5, height=1, relief=RAISED, bd=2, command=lambda: show_value('-')).place(x=310, y=180)
-Button(window, text="0", bg="#B3B6B7", fg="white", font=("Arial", 20, "bold"), width=11, height=1, relief=RAISED, bd=2, command=lambda: show_value('0')).place(x=10, y=390)
-Button(window, text=".", bg="#B3B6B7", fg="white", font=("Arial", 20, "bold"), width=5, height=1, relief=RAISED, bd=2, command=lambda: show_value('.')).place(x=210, y=390)
-
-window.mainloop()
+import tkinter root = tkinter.Tk() 
+root.title("Calculator") 
+expression = "" def add(value): 
+    global expression     expression += value 
+    label_result.config(text=expression)
+ def clear():     
+	global expression     
+	expression = "" 
+    	label_result.config(text=expression)
+ def calculate(): 
+    	global expression     
+	result = ""     
+	if expression != "":         
+	try: 
+      result = eval(expression)         
+	except:            
+	result = "error"             
+	expression = "" 
+    	label_result.config(text=result) label_result = tkinter.Label(root, text="") label_result.grid(row=0, column=0, columnspan=4) 
+	button_1 = tkinter.Button(root, text="1", command=lambda: add("1")) button_1.grid(row=1, column=0) 
+	button_2 = tkinter.Button(root, text="2", command=lambda: add("2")) button_2.grid(row=1, column=1) 
+	button_3 = tkinter.Button(root, text="3", command=lambda: add("3")) button_3.grid(row=1, column=2) 
+ 	button_divide = tkinter.Button(root, text="/", command=lambda: add("/")) button_divide.grid(row=1, column=3) 
+	button_4 = tkinter.Button(root, text="4", command=lambda: add("4")) button_4.grid(row=2, column=0) 
+	button_5 = tkinter.Button(root, text="5", command=lambda: add("5")) button_5.grid(row=2, column=1) 
+	button_6 = tkinter.Button(root, text="6", command=lambda: add("6")) button_6.grid(row=2, column=2) 
+	button_multiply = tkinter.Button(root, text="*", command=lambda: add("*")) button_multiply.grid(row=2, column=3) 
+	button_7 = tkinter.Button(root, text="7", command=lambda: add("7")) button_7.grid(row=3, column=0) 
+	button_8 = tkinter.Button(root, text="8", command=lambda: add("8")) button_8.grid(row=3, column=1) 
+	button_9 = tkinter.Button(root, text="9", command=lambda: add("9")) button_9.grid(row=3, column=2) 
+	button_subtract = tkinter.Button(root, text="-", command=lambda: add("-")) button_subtract.grid(row=3, column=3) 
+	button_clear = tkinter.Button(root, text="C", command=lambda: clear()) button_clear.grid(row=4, column=0) 
+	button_0 = tkinter.Button(root, text="0", command=lambda: add("0")) button_0.grid(row=4, column=1) 
+	button_dot = tkinter.Button(root, text=".", command=lambda: add(".")) button_dot.grid(row=4, column=2) 
+	button_add = tkinter.Button(root, text="+", command=lambda: add("+")) button_add.grid(row=4, column=3) button_equals = tkinter.Button(root, text="=", width=16, command=lambda: 
+	calculate()) 
+	button_equals.grid(row=5, column=0, columnspan=4) root.mainloop() 
